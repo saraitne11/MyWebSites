dcf67017652f3d1a5e08415367056922def74260
Jangwon Lee <saraitne11@naver.com>
2022-04-29 15:17:12 +0900

    Update main.js

--- OpenTutorialsCourses/NodeJS/main.js
+++ OpenTutorialsCourses/NodeJS/main.js
@@ -2,53 +2,65 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');     // url module
 
+function getTemplateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function getTemplateList(fileList){
+  var listStr = '<ul>';
+  var i = 0
+  while(i < fileList.length){
+    listStr = listStr + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+    i = i + 1;
+  }
+  listStr = listStr + '</ul>';
+  return listStr;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathName = url.parse(_url, true).pathname;
-    var articlesDir = 'articles/'
+    var articleDir = './articles/'
 
 
     if(pathName === '/'){
-      var fileList = fs.readdirSync(articlesDir);
-      var contentsList = '<ul>';
-      var i = 0
-      while(i < fileList.length){
-        contentsList = contentsList + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-        i = i + 1;
-      }
-      contentsList = contentsList + '</ul>';
       
       if(queryData.id === undefined){
-        var title = 'Welcome';
-        var article = 'Hello, Node.JS!';
+        fs.readdir(articleDir, function(err, fileList){
+          var title = 'Welcome';
+          var article = 'Hello, Node.JS!';
+          var list = getTemplateList(fileList);
+          var body = `<h2>${title}</h2><p>${article}</p>`;
+          var template = getTemplateHTML(title, list, body);
+          response.writeHead(200);
+          response.end(template);
+        })
       } else {
-        var title = queryData.id;
-        try {
-          var article = fs.readFileSync(articlesDir + queryData.id, {encoding: 'utf-8', flag: 'r'});
-        } catch (err){
-          if (err.code !== 'ENOENT') throw err;   // No File Not Found Error
-          else var article = `File [${queryData.id}] Not Found!`    // It's File Not Fount Error
-        }
+        fs.readdir(articleDir, function(err1, fileList){
+          var title = queryData.id;
+          fs.readFile(articleDir + queryData.id, 'utf-8', function(err2, article){
+            var list = getTemplateList(fileList);
+            var body = `<h2>${title}</h2><p>${article}</p>`;
+            var template = getTemplateHTML(title, list, body);
+            response.writeHead(200);
+            response.end(template);
+          })
+        })
       }
-
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB2 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</ a></h1>
-        ${contentsList}
-        <h2>${title}</h2>
-        <p>${article}</p>
-      </body>
-      </html>
-      `;
-        response.writeHead(200);
-        response.end(template);
       
     } else {
       response.writeHead(404);
